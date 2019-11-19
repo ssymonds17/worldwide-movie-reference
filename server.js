9d4574f3e866f47c4f92b14783d1ba73d1ca57fc
Sam Symonds <samuelsymonds2017@gmail.com>
2019-11-19 17:07:50 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
+
+http.createServer((request, response) => {
+  var addr = request.url,
+  q = url.parse(addr, true),
+  filePath = '';
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (_dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' +
+   new Date() + '\n\n', function(err) {
+     if (err) {
+       console.log(err);
+     } else {
+       console.log('Added to log.');
+     }
+   })
+}).listen(8080);

bc8960149033e51f08dbbb7af9a0d50920b5ac40
Sam Symonds <samuelsymonds2017@gmail.com>
2019-11-20 11:03:05 +0000

    Changed var to const and added a response from server
    This commit will change the variables defined by var to const. It also has added a response from the server.
    Finally notes have been added as well.

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
+// Imports modules for http, fs and url
 const http = require('http'),
   fs = require('fs'),
   url = require('url');
 
+// Sets up the local server
 http.createServer((request, response) => {
-  var addr = request.url,
+  const addr = request.url,
   q = url.parse(addr, true),
   filePath = '';
 
+  // If pathname includes documentation then
+  // return the documentation/html file, if not then index.html
   if (q.pathname.includes('documentation')) {
     filePath = (_dirname + '/documentation.html');
   } else {
     filePath = 'index.html';
   }
 
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, {'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  });
+
+  // Add instance of server request to log.txt file with URL and timestamp
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' +
    new Date() + '\n\n', function(err) {
      if (err) {
@@ -21,4 +36,6 @@ http.createServer((request, response) => {
        console.log('Added to log.');
      }
    })
+
+// Port 8080
 }).listen(8080);

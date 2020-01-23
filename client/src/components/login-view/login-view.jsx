53da8a34ef78fa2d163739e99884653bea4acd95
Sam Symonds <samuelsymonds2017@gmail.com>
2020-01-23 18:44:29 +0000

    Added <Link> and handleSubmit functionality

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -7,6 +7,9 @@ import axios from 'axios';
 import MyNavbar from '../navbar/navbar';
 import './login-view.scss';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -29,34 +32,37 @@ export function LoginView(props) {
 
   return (
 
-    <div>
-      <MyNavbar />
-      <Container className="loginContainer">
-        <h1>Login</h1>
-        <Form>
-          <Form.Group controlId="formUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Choppy1999" value={username} onChange={e => setUsername(e.target.value)} />
-          </Form.Group>
-          <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="text" placeholder="abcd1234" value={password} onChange={e => setPassword(e.target.value)} />
-          </Form.Group>
-          <Button variant="primary" onClick={handleSubmit}>Log In</Button>
-        </Form>
-        <br />
-        <Form>
-          <Form.Group controlId="newUser">
-            <Form.Text>Not a member yet?</Form.Text>
-            <Button variant="secondary" id="registerButton" onClick={() => props.onClick()}>Register</Button>
-          </Form.Group>
-        </Form>
-      </Container>
-    </div>
+    <Router>
+      <div>
+        <MyNavbar />
+        <Container className="loginContainer">
+          <h1>Login</h1>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Choppy1999" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="text" placeholder="abcd1234" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" onClick={handleSubmit}>Log In</Button>
+          </Form>
+          <br />
+          <Form>
+            <Form.Group controlId="newUser">
+              <Form.Text>Not a member yet?</Form.Text>
+              <Link to={`/register`}>
+                <Button variant="secondary" id="registerButton">Register</Button>
+              </Link>
+            </Form.Group>
+          </Form>
+        </Container>
+      </div>
+    </Router>
   )
 }
 
 LoginView.propTypes = {
   onLoggedIn: PropTypes.func.isRequired,
-  onClick: PropTypes.func.isRequired
 };

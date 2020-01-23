4b2c6c08a69140b6deaf6330566407e90e6d378f
Sam Symonds <samuelsymonds2017@gmail.com>
2020-01-23 18:38:02 +0000

    Reinserted jwt authentication in index.js
    This commit returned the code that verifies my API requests for the /movies endpoint

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
 });
 
 // GET request to return a list of ALL movies
-app.get('/movies', function (req, res) {
+app.get('/movies', passport.authenticate('jwt', { session: false }), function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies)

d9ed9a3819d1539565a960ba362100a05b59eeab
Sam Symonds <samuelsymonds2017@gmail.com>
2020-01-23 18:42:20 +0000

    Updated button to go to MovieView
    New button to link to MovieView. <Link> instead of onClick()

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -4,12 +4,11 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss';
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
   render() {
-    // This is given to the <MovieCard/> component by the outer world
-    // which, in this case, is the 'MainView', as 'MainView' is what's
-    // connected to my database via the movies endpoint of my API
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     return (
       <Card className="movieCardContainer" style={{ minWidth: '12rem' }}>
@@ -18,7 +17,9 @@ export class MovieCard extends React.Component {
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Card.Text>{movie.description}</Card.Text>
-          <Button onClick={() => onClick(movie)} variant="outline-primary link">Open</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link outline-primary">More</Button>
+          </Link>
         </Card.Body>
       </Card>
     );
@@ -32,5 +33,4 @@ MovieCard.propTypes = {
     description: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired
   }).isRequired,
-  onClick: PropTypes.func.isRequired
 };

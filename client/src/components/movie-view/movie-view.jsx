1b289ad3025904a8f1871fdc42b8fcb7b0d95694
Sam Symonds <samuelsymonds2017@gmail.com>
2020-01-23 18:41:26 +0000

    Updated MovieView
    Created addToFaveList function which adds a favourite movie.
    There are now buttons to link to the specific genre and director of the film in question. But this is still to be completed

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './movie-view.scss';
+
+import { Link } from 'react-router-dom';
 
 export class MovieView extends React.Component {
 
@@ -11,26 +14,55 @@ export class MovieView extends React.Component {
     this.state = {};
   }
 
+  addToFaveList(event) {
+    const { movie } = this.props;
+    event.preventDefault();
+    axios.post(`https://worldwide-movie-reference.herokuapp.com/users/${localStorage.getItem('user')}/movies/${movie._id}`, {
+      Username: localStorage.getItem('user')
+    }, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+      .then(response => {
+        console.log(response);
+        alert('Movie has been added to list of favourites');
+      })
+      .catch(event => {
+        console.log('error adding movie to list');
+        alert('Something went wrong');
+      });
+  };
+
   render() {
-    const { movie, previous } = this.props;
+    const { movie } = this.props;
 
     if (!movie) return null;
 
     return (
 
-      <Card className="movieViewContainer" style={{ width: '75%' }}>
+      <Card className="movie-view-container" style={{ width: '75%' }} >
         {/* <Card.Img variant="top" src={movie.imagePath} /> */}
-        <Card.Img className="movieViewImage" variant="top" src="http://via.placeholder.com/640x360" />
+        < Card.Img className="movie-view-image" variant="top" src="http://via.placeholder.com/640x360" />
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Card.Text>{movie.year}</Card.Text>
           <Card.Text>Description: {movie.description}</Card.Text>
           <Card.Text>Run Time: {movie.length} minutes</Card.Text>
-          <Card.Text>Genre: {movie.genre}</Card.Text>
-          <Card.Text>Director: {movie.director}</Card.Text>
-          <Button variant="outline-secondary" className="back-button" onClick={() => previous()}>Back</Button>
+          <Card.Text>Genre:
+            <Link to={`/genres/${movie.genre.name}`}>
+              <Button className="genre-link-button" variant="link">{movie.genre}</Button>
+            </Link>
+          </Card.Text>
+          <Card.Text>Director:
+            <Link to={`/directors/${movie.director.name}`}>
+              <Button className="director-link-button" variant="link">{movie.director}</Button>
+            </Link>
+          </Card.Text>
+          <Button onClick={(event) => this.addToFaveList(event)}>Add movie to favourites</Button>
+          <Link to={`/`}>
+            <Button variant="outline-secondary" className="back-button">Back</Button>
+          </Link>
         </Card.Body>
-      </Card>
+      </Card >
     );
   }
 }
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -7,27 +7,30 @@ import Button from 'react-bootstrap/Button';
 import './navbar.scss';
 
 function MyNavbar() {
+
   return (
-    <Navbar className="navbarContainer" bg="primary" expand="lg" fixed="top">
-      <Navbar.Brand className="navBrand" href="#home">WorldWide Movie Reference</Navbar.Brand>
-      <Navbar.Toggle aria controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown title="Menu" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#movies">Movies</NavDropdown.Item>
-            <NavDropdown.Item href="#genres">Genres</NavDropdown.Item>
-            <NavDropdown.Item href="#directors">Directors</NavDropdown.Item>
-            <NavDropdown.Item href="#actors">Actors</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#login">Log In</Nav.Link>
-          <Nav.Link href="#register">Register</Nav.Link>
-        </Nav>
-        <Form inline>
-          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <Navbar className="navbarContainer" bg="primary" expand="lg" fixed="top">
+        <Navbar.Brand className="navBrand" href="#home">WorldWide Movie Reference</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#movies">Movies</NavDropdown.Item>
+              <NavDropdown.Item href="#genres">Genres</NavDropdown.Item>
+              <NavDropdown.Item href="#directors">Directors</NavDropdown.Item>
+              <NavDropdown.Item href="#actors">Actors</NavDropdown.Item>
+            </NavDropdown>
+            <Button className="logout-button" onClick={() => this.onLoggedOut()}>Log Out</Button>
+            <Nav.Link href="/register">Register</Nav.Link>
+            {/* <Nav.Link href={`/users/${user.username}`} */}
+          </Nav>
+          <Form inline>
+            <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 

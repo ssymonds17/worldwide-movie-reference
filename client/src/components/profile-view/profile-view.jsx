4c42b89ba38d534a577d1afa434700b37a4aaf4b
Sam Symonds <samuelsymonds2017@gmail.com>
2020-02-10 23:34:18 +0000

    Removed window.open from faveDelete function
    This commit removes the window.open aspect of the handleDelete function

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -70,7 +70,7 @@ export class ProfileView extends React.Component {
         })
             .then(response => {
                 console.log(token);
-                window.open(`/client/users/${localStorage.getItem('user')}`, '_self');
+                // window.open(`/client/users/${localStorage.getItem('user')}`, '_self');
                 alert('Movie has been removed from list of favourites');
             })
             .catch(error => {

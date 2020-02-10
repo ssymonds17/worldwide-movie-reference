136745e69c72df74f3e611c67e5fa78006f2e13f
Sam Symonds <samuelsymonds2017@gmail.com>
2020-02-10 19:33:48 +0000

    Updated paths

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -100,16 +100,12 @@ export class MainView extends React.Component {
         <Container className="main-view">
           <div>
             <Navbar className="navbarContainer mb-5" bg="primary" expand="md" fixed="top">
-              <Navbar.Brand className="nav-brand" href="/">WorldWide Movie Reference</Navbar.Brand>
-              <Link to={`/users/${user}`}>{user}</Link>
+              <Navbar.Brand className="nav-brand" href="/client">WorldWide Movie Reference</Navbar.Brand>
+              <Button href={`client/users/${user}`}>{user}</Button>
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                  <Link to={`/`}>
-                    <Button className="logout-button" type="primary" onClick={user => this.onLoggedOut(user)}>Log Out</Button>
-                  </Link>
-                  <Link to={`/register`}>
-                    <Button type="primary">Register</Button>
-                  </Link>
+                  <Button className="logout-button" onClick={user => this.onLoggedOut(user)}>Log Out</Button>
+                  <Button href="/client/register">Register</Button>
                   <NavDropdown title="Menu" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#movies">Movies</NavDropdown.Item>
                     <NavDropdown.Item href="#genres">Genres</NavDropdown.Item>
